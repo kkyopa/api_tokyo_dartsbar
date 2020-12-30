a46f9e8aba373d905683c0d78c12d182292bb09b
川田京助 <kkyopa@gmail.com>
2020-12-30 18:13:02 +0900

    テスト

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const todoList = [
+    { title: 'JavaScriptを勉強する', done: true },
+    { title: 'Node.jsを勉強する', done: false },
+    { title: 'Web APIを作る', done: false }
+];
+res.json(todoList);
 });
 
 module.exports = router;
