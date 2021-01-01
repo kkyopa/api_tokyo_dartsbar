8ea30e880a75d08c159515d818fc2652247271e8
川田京助 <kkyopa@gmail.com>
2021-01-01 10:55:09 +0900

    テストデータ作成

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,48 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var cors = require("cors");
+var corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
+
+// router.get('/api/sample', cors(corsOptions), function(req, res, next) {
+//   res.json('success');
+// });
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  const todoList = [
-    { title: 'JavaScriptを勉強する', done: true },
-    { title: 'Node.jsを勉強する', done: false },
-    { title: 'Web APIを作る', done: false }
-];
-res.json(todoList);
+router.get("/", cors(corsOptions), function (req, res, next) {
+  (todo = {
+    kind: "aaa",
+    totalItems: 3026,
+    items: [
+      {
+        id: 1,
+        status: {
+          title: "スリーコード",
+          url:
+            "https://search.dartslive.com/jp/shop/674ed91d86ef74ab0d9b047a20a7ba1e",
+        },
+      },
+      {
+        id: 2,
+        status: {
+          title: "zenon",
+          url:
+            "https://search.dartslive.com/jp/shop/3d92d05c396c51860d9b047a20a7ba1e",
+        },
+      },
+      {
+        id: 3,
+        status: {
+          title: "中野UP",
+          url:
+            "https://search.dartslive.com/jp/shop/837e77cc45ca3c785f9f3321c1147265",
+        },
+      },
+    ],
+  }),
+    res.json(todo);
 });
 
 module.exports = router;
